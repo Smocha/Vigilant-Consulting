0d448e08825dfe02b53956258ed751302bdac5a3
Smocha <vronnell@hotmail.com>
2026-05-29 17:04:31 -0700

    Delete components/Footer.tsx

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-brand-dark border-t border-gray-700 text-gray-400 py-6 px-4">
-      <div className="container mx-auto text-center text-sm">
-        <p>&copy; {new Date().getFullYear()} Vigilant Tech. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-}
